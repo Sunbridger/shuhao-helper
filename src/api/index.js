d4b58798a910304bb79da9c973f1f16ec3b391c0
Sunbridger <739272884@qq.com>
2019-11-01 15:52:10 +0800

    fix: 添加环境url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseUrl = process.env.VUE_APP_DOMAIN;
-console.log(process.env, 'baseUrl');
+console.log(baseUrl, 'baseUrl');
 export const get = (url, opt = {}) => {
     return axios.get(baseUrl + url, opt);
 }
--- vue.config.js
+++ vue.config.js
@@ -3,16 +3,5 @@ module.exports = {
         resolve: {
             extensions: ['.vue', '.js', '.ts', '.json']
         }
-    },
-    devServer: {
-        proxy: {
-            '/eggapi': {
-                target: 'http://47.96.149.250:7001',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/eggapi': ''
-                }
-            }
-        }
     }
 }
