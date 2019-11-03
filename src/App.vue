602c9173befcb4683236b2aee4fd9552b32cddf2
Sunbridger <739272884@qq.com>
2019-11-03 19:17:05 +0800

    fix: 更新

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,6 @@
   <div id="app">
       <h1>网站待更新...</h1>
       <img src="http://img.soogif.com/yOaiRUMu16q1X30YfhK6obpQNaqd2p50.gif_s400x0" alt="">
-      
-      
   </div>
 </template>
 
@@ -26,16 +24,19 @@ export default class App extends Vue {
     isalways = '写法方便点咯';
     students: Array<Person> = [];
     created() {
-        // get('/').then(res => {
-        //     this.students = res.data;
-        // })
+        get('/add', {
+            name: 'Sunbridger',
+            age: 23
+        }).then(res => {
+            console.log(res);
+        })
     }
     getData(row) {
         this.isnormal = + new Date();
     }
     @Watch('isnormal') 
-    watcFn(a, b) {
-        console.log(99999, a, b);
+    watcisnormalFn(a, b) {
+        console.log('这个名字随意 可以直接匿名么', a, b);
     }
     add() {
         get('/add').then(res => {
