d053b8f95c16bacb7bc9dad3446d0b043ea693d5
Sunbridger <739272884@qq.com>
2019-11-11 16:01:20 +0800

    fix: 修改

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,14 +1,31 @@
 <template>
     <div>
-        <el-input v-model="input" placeholder="请输入内容..."></el-input>
+        <el-input placeholder="请输入姓名" v-model="name" maxlength="10" show-word-limit></el-input>
+        <el-input type="number" placeholder="请输入年龄" v-model="age" ></el-input>
+        <el-button type="info" icon="el-icon-message" circle @click="submit"></el-button>
     </div>
 </template>
-<script lang="ts">
+<script>
+
+import { get } from '../api';
+
 export default {
     name: 'login',
     data() {
         return {
-            input: ''
+            name: '',
+            age: ''
+        }
+    },
+    methods: {
+        submit() {
+            const { name, age } = this;
+            get('/add', {
+                name,
+                age
+            }).then(res => {
+                this.$message('ok');
+            });
         }
     }
 };
