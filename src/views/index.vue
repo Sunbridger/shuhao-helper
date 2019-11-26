4ff301d84e52af7f19b8a8a2821bd19076430b5d
Sunbridger <739272884@qq.com>
2019-11-26 11:36:39 +0800

    fix: 去除多余代码

--- src/views/index.vue
+++ src/views/index.vue
@@ -20,12 +20,9 @@
     <div>
         <div class="index-box">
             <el-tabs v-model="activeName" @tab-click="handleClick">
-                <!-- <el-tab-pane label="榜姐评论排行" name="/login"></el-tab-pane> -->
-                <!-- <el-tab-pane label="热点微博" name="/me"></el-tab-pane> -->
-                <el-tab-pane label="监视淘宝商品" name="/watchTb"></el-tab-pane>
-                <el-tab-pane label="淘宝数据" name="/taobao"></el-tab-pane>
+                <el-tab-pane label="监视TB商品" name="/watchTb"></el-tab-pane>
+                <el-tab-pane label="收藏商品" name="/taobao"></el-tab-pane>
                 <el-tab-pane label="热榜可视化" name="/reban"></el-tab-pane>
-                <!-- <el-tab-pane label="微博热搜" name="/hot"></el-tab-pane> -->
             </el-tabs>
         </div>
         <keep-alive>
