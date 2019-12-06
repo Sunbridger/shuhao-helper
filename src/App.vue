981b5c8aa188327eb548af65c5cee527730b5a42
Sunbridger <739272884@qq.com>
2019-12-06 17:41:14 +0800

    fix: 测试h5+能力

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,30 @@
 
 <template>
     <div id="app">
+        <div @click="share">分享</div>
         <router-view></router-view>
     </div>
 </template>
 
-<script lang="ts">
+<script>
 
 import VueRouter from 'vue-router';
-
+import '../plusShare';
 export default {
+    methods: {
+        share() {
+            if(navigator.userAgent.indexOf("Html5Plus") > -1) {
+                //5+ 原生分享
+                window.plusShare({
+                    title: "Sunbridger的app",//应用名字
+                    content: "很好玩的东西哦",
+                    href: location.href,//分享出去后，点击跳转地址
+                    thumbs: ["https://lh3.googleusercontent.com/-JO6aXzDjbGw/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rd1rskfzc7PVxXgGWHOSQjGkEhJ4g.CMID/s64-c/photo.jpg"] //分享缩略图
+                }, function(result) {
+                    this.$message('分享成功咯');
+                });
+            }
+        }
+    }
 }
 </script>
