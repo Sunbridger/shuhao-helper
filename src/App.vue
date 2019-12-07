bb8af1ae715d94b98f58bfddb52ad3b717790738
Sunbridger <739272884@qq.com>
2019-12-07 20:14:41 +0800

    fix: 测试h5+能力

--- src/App.vue
+++ src/App.vue
@@ -28,10 +28,10 @@ export default {
             }
         },
         created() {
-            if (navigator.userAgent.indexOf("Html5Plus") > -1) {
-                alert(JSON.stringify(plus))
-                plus.nativeUI.alert('message', this.share, '标题处哦', '按钮显示内容');
-            }
+            // if (navigator.userAgent.indexOf("Html5Plus") > -1) {
+            //     alert(JSON.stringify(plus))
+            //     plus.nativeUI.alert('message', this.share, '标题处哦', '按钮显示内容');
+            // }
         }
     }
 }
