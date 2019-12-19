4be96f52fda5c5a85952fef3a2157b61251f7e18
Sunbridger <739272884@qq.com>
2019-12-19 21:21:32 +0800

    fix: 去除注释

--- src/App.vue
+++ src/App.vue
@@ -14,25 +14,25 @@ import VueRouter from 'vue-router';
 import '../plusShare';
 export default {
     methods: {
-        share() {
-            if(navigator.userAgent.indexOf("Html5Plus") > -1) {
-                //5+ 原生分享
-                window.plusShare({
-                    title: "Sunbridger的app",//应用名字
-                    content: "很好玩的东西哦",
-                    href: location.href,//分享出去后，点击跳转地址
-                    thumbs: ["https://lh3.googleusercontent.com/-JO6aXzDjbGw/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rd1rskfzc7PVxXgGWHOSQjGkEhJ4g.CMID/s64-c/photo.jpg"] //分享缩略图
-                }, function(result) {
-                    this.$message('分享成功咯');
-                });
-            }
-        },
-        created() {
+        // share() {
+        //     if(navigator.userAgent.indexOf("Html5Plus") > -1) {
+        //         //5+ 原生分享
+        //         window.plusShare({
+        //             title: "Sunbridger的app",//应用名字
+        //             content: "很好玩的东西哦",
+        //             href: location.href,//分享出去后，点击跳转地址
+        //             thumbs: ["https://lh3.googleusercontent.com/-JO6aXzDjbGw/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rd1rskfzc7PVxXgGWHOSQjGkEhJ4g.CMID/s64-c/photo.jpg"] //分享缩略图
+        //         }, function(result) {
+        //             this.$message('分享成功咯');
+        //         });
+        //     }
+        // },
+        // created() {
             // if (navigator.userAgent.indexOf("Html5Plus") > -1) {
             //     alert(JSON.stringify(plus))
             //     plus.nativeUI.alert('message', this.share, '标题处哦', '按钮显示内容');
             // }
-        }
+        // }
     }
 }
 </script>
