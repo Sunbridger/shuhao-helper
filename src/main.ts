e37a439a906310e61b765fafe4f13e695411fb27
Sunbridger <739272884@qq.com>
2020-01-15 18:44:04 +0800

    feat: 外部用cdn css

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
 import VueAwesomeSwiper from 'vue-awesome-swiper';
-import 'swiper/dist/css/swiper.css'
+// import 'swiper/dist/css/swiper.css'
 import './plugins/element';
 import router from './router'
 
