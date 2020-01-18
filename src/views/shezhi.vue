2035c9f643458097b219d25781b60915e504f71d
Sunbridger <739272884@qq.com>
2020-01-18 17:00:29 +0800

    fix: 修改

--- src/views/shezhi.vue
+++ src/views/shezhi.vue
@@ -41,7 +41,7 @@
 export default {
     data() {
         const care = localStorage.getItem('care') === 'true' ? true : false;
-        const sort = localStorage.getItem('sort') === 'true' ? false : true;
+        const sort = localStorage.getItem('sort') === 'true' ? true : false;
         const isLogin = localStorage.getItem('isLogin') === 'true' ? false : true;
         return {
             care,
