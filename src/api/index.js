14b513111cc45ef20c817a6f37f09545a70c4401
七彩 <wangriqiao@cai-inc.com>
2021-11-29 17:37:21 +0800

    feat: 暂时修改域名为host

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.VUE_APP_DOMAIN;
+const baseUrl = 'http://119.45.209.166:7002' || process.env.VUE_APP_DOMAIN;
 export const get = (url, params = {}) => {
     return axios.get(baseUrl + url, {
         params
